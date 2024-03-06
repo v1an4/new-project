97a0c4bef1624605d333fc78d8b70db085329e18
v1an4 <159141566+v1an4@users.noreply.github.com>
2024-03-05 22:16:46 -0300

    Add files via upload

---
+++ new-project/pedido/scrpit.js
@@ -0,0 +1,21 @@
+function comecar(){
+    var nome = document.getElementById("exampleFormControlInput1").value;
+    show(nome);
+ 
+ }
+ 
+ function show(nome){
+    document.getElementById("nomeprint").textContent = nome;
+    var div = document.getElementById("selects-dic");
+    if(nome && nome.trim() !== ""){
+        div.style.display = "block"
+        document.getElementById("exampleFormControlInput1").style.background = "none";
+    }else{
+        document.getElementById("error").style.display = "flex"
+    }
+    
+ }
+ 
+ function fechar(){
+    document.getElementById("error").sty
+ }
